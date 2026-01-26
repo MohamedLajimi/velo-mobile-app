6bc16f062d0a1218fa732f50ec6e629c3d38bf46
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-01-26 16:08:19 +0100

    feat(theme): setup app colors, theme and theme extension

---
+++ lib/core/extensions/theme_extension.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+extension ThemeContext on BuildContext {
+  ThemeData get theme => Theme.of(this);
+
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+
+  TextTheme get textTheme => Theme.of(this).textTheme;
+
+  double get screenWidth => MediaQuery.sizeOf(this).width;
+  
+  double get screenHeight => MediaQuery.sizeOf(this).height;
+}
