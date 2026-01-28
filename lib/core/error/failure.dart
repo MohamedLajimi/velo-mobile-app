40b486707cb599a4ceaae5ca209dd34e883ed9a9
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-01-28 16:34:17 +0100

    feat(core): add cache failure class for shared prefs exceptions

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -11,6 +11,11 @@ class DatabaseFailure extends Failure {
   const DatabaseFailure(super.message);
 }
 
+class CacheFailure extends Failure {
+  const CacheFailure(super.message);
+}
+
+
 class AuthFailure extends Failure {
   const AuthFailure(super.message);
 }
