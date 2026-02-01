923d362d45d2a54d7ff1f35a944596989ba06b5d
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-02-01 18:50:41 +0100

    feat(core): Adds language selection and persistent localization
    Introduces language selection UI and state management for localization,
    persisting user language preference across sessions. Updates translation
    files with additional authentication-related strings to support new
    UI and error messages. Refactors app initialization to respect stored
    language and enables switching between English and French dynamically.

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -3,7 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:karaba/core/common/blocs/language_cubit/language_cubit.dart';
+import 'package:karaba/core/routes/app_router.dart';
+import 'package:karaba/core/services/storage_service.dart';
 import 'package:karaba/env_config.dart';
+import 'package:karaba/features/auth/auth_injection.dart';
 import 'package:karaba/features/onboarding/onboarding_injection.dart';
 import 'package:karaba/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -12,26 +16,35 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  await dotenv.load(fileName: ".env");
+  await Future.wait([
+    dotenv.load(fileName: ".env"),
+    EasyLocalization.ensureInitialized(),
+    SharedPreferences.getInstance().then(
+      (prefs) => sl.registerLazySingleton(() => prefs),
+    ),
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+  ]);
 
   await Supabase.initialize(
     url: EnvConfig.supabaseUrl,
     anonKey: EnvConfig.supabaseAnonKey,
   );
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  sl.registerLazySingleton(() => Supabase.instance.client);
 
   await GoogleSignIn.instance.initialize(
     serverClientId: EnvConfig.serverClientId,
   );
 
-  await EasyLocalization.ensureInitialized();
+  sl.registerLazySingleton(() => StorageService(supabaseClient: sl()));
 
-  final sharedPrefs = await SharedPreferences.getInstance();
-
-  sl.registerLazySingleton(() => sharedPrefs);
-
-  sl.registerLazySingleton(() => Supabase.instance.client);
+  sl.registerLazySingleton(() => LanguageCubit(sharedPreferences: sl()));
 
   initOnboarding(sl);
+
+  initAuth(sl);
+
+  sl.registerLazySingleton(
+    () => AppRouter(userCubit: sl(), onboardingBloc: sl()),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:karaba/bloc_observer.dart';
+import 'package:karaba/core/common/blocs/language_cubit/language_cubit.dart';
+import 'package:karaba/core/common/blocs/user_cubit/user_cubit.dart';
 import 'package:karaba/core/di/injection_container.dart';
+import 'package:karaba/core/routes/app_router.dart';
 import 'package:karaba/core/theme/theme.dart';
 import 'package:karaba/features/onboarding/presentation/bloc/onboarding_bloc.dart';
-import 'package:karaba/features/onboarding/presentation/screens/onboarding_screen.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -16,11 +19,14 @@ Future<void> main() async {
 
   FlutterNativeSplash.remove();
 
+  Bloc.observer = AppBlocObserver();
+
   runApp(
     EasyLocalization(
       supportedLocales: [Locale('en'), Locale('fr')],
       path: 'assets/translations',
       fallbackLocale: Locale('en', 'US'),
+      startLocale: sl<LanguageCubit>().state.locale,
       child: const MyApp(),
     ),
   );
@@ -31,17 +37,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Karaba',
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      builder: (context, child) =>
-          Theme(data: AppTheme.getTheme(context), child: child!),
-      home: BlocProvider(
-        create: (context) => sl<OnboardingBloc>(),
-        child: const OnboardingScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: sl<UserCubit>()),
+        BlocProvider.value(value: sl<OnboardingBloc>()),
+        BlocProvider.value(value: sl<LanguageCubit>()),
+      ],
+      child: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        behavior: HitTestBehavior.opaque,
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'Karaba',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          builder: (context, child) =>
+              Theme(data: AppTheme.getTheme(context), child: child!),
+          routerConfig: sl<AppRouter>().router,
+        ),
       ),
     );
   }
