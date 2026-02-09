6bf4b55f691875e6da2aa8a81130b63109d5e1d5
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-02-09 14:49:58 +0100

    feat(auth): Refactor checkAuthStatus and updateUser methods for improved readability

--- lib/core/common/blocs/user_cubit/user_cubit.dart
+++ lib/core/common/blocs/user_cubit/user_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:karaba/core/common/entities/user_entity.dart';
+import 'package:karaba/core/error/failure.dart';
 import 'package:karaba/features/auth/domain/usecases/get_current_user_use_case.dart';
 
 part 'user_state.dart';
@@ -19,20 +20,17 @@ class UserCubit extends Cubit<UserState> {
 
     final result = await _getCurrentUserUseCase.call();
 
-    result.fold((failure) => emit(UserError(failure.message)), (user) {
-      if (user != null) {
-        emit(UserAuthenticated(user));
-      } else {
-        emit(UserUnauthenticated());
-      }
-    });
+    result.fold(
+      (failure) => failure is! NoSessionFailure
+          ? emit(UserError(failure.message))
+          : emit(UserUnauthenticated()),
+      (user) => user != null
+          ? emit(UserAuthenticated(user))
+          : emit(UserUnauthenticated()),
+    );
   }
 
-  void updateUser(UserEntity? user) {
-    if (user == null) {
-      emit(UserUnauthenticated());
-    } else {
-      emit(UserAuthenticated(user));
-    }
-  }
+  void updateUser(UserEntity? user) => user == null
+      ? emit(UserUnauthenticated())
+      : emit(UserAuthenticated(user));
 }
