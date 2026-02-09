79072d027883d8f0db247c7c0f35ad1191b2cafe
=Mohamed Lajimi <=med.lajimi11@gmail.com>
2026-02-09 14:55:48 +0100

    feat(auth): Add complete profile route and implement CompleteProfileScreen in AuthRoutes

--- lib/features/auth/presentation/routes/auth_routes.dart
+++ lib/features/auth/presentation/routes/auth_routes.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:karaba/core/di/injection_container.dart';
 import 'package:karaba/features/auth/presentation/blocs/auth_bloc/auth_bloc.dart';
 import 'package:karaba/features/auth/presentation/blocs/reset_password_cubit/reset_password_cubit.dart';
+import 'package:karaba/features/auth/presentation/screens/complete_profile_screen.dart';
 import 'package:karaba/features/auth/presentation/screens/sign_in_screen.dart';
 import 'package:karaba/features/auth/presentation/screens/reset_password_screen.dart';
 import 'package:karaba/features/auth/presentation/screens/sign_up_screen.dart';
@@ -14,6 +15,9 @@ class AuthRoutes {
   static const String signupPath = '/auth/signup';
   static const String resetPasswordName = 'reset-password';
   static const String resetPasswordPath = '/auth/reset-password';
+  static const String completeProfileName = '/auth/complete-profile';
+  static const String completeProfilePath = '/auth/complete-profile/:id';
+  static String completeProfilePathWithId(String id) => '/auth/complete-profile/$id';
 
   static List<RouteBase> routes = [
     GoRoute(
@@ -39,6 +43,18 @@ class AuthRoutes {
         create: (context) => sl<ResetPasswordCubit>(),
         child: const ResetPasswordScreen(),
       ),
+    ),
+
+    GoRoute(
+      name: completeProfileName,
+      path: completeProfilePath,
+      builder: (context, state) {
+        final userId = state.pathParameters['id'] as String;
+        return BlocProvider(
+          create: (context) => sl<AuthBloc>(),
+          child: CompleteProfileScreen(userId: userId),
+        );
+      },
     ),
   ];
 }
